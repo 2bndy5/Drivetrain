d8ba7cebfdcd8d61395085db7b7aa1fcbf97a26a
Brendan <2bndy5@gmail.com>
2019-11-21 03:49:42 -0800

    added comments to nrf24 rx example

--- examples/nrf24l01_rx_test.py
+++ examples/nrf24l01_rx_test.py
@@ -1,11 +1,16 @@
+"""
+Example of library usage receiving commands via an
+nRF24L01 transceiver to control a Mecanum drivetrain.
+"""
 import board
 from digitalio import DigitalInOut as Dio
 from circuitpython_nrf24l01 import RF24
 from drivetrain import Mecanum, BiMotor, NRF24L01rx
 
-SPI_BUS = board.SPI()
-nrf = RF24(SPI_BUS, Dio(board.D5), Dio(board.D4))
+# instantiate transceiver radio on the SPI bus
+nrf = RF24(board.SPI(), Dio(board.D5), Dio(board.D4))
 
+# instantiate motors for a Mecanum drivetrain in the following order
 # Front-Right, Rear-Right, Rear-Left, Front-Left
 motors = [
     BiMotor([board.RX, board.TX]),
@@ -13,8 +18,13 @@ motors = [
     BiMotor([board.D11, board.D10]),
     BiMotor([board.D2, board.D7])
     ]
+# NOTE there are no more PWM pins available
 
+# instantiate receiving object for a Mecanum drivetrain
 d = NRF24L01rx(nrf, Mecanum(motors))
 
-while True:
+while True: # this runs forever
     d.sync()
+# doing a keyboard interupt will most likely leave the SPI bus in an
+# undesirable state. You must do a hard-reset of the circuitoython MCU to
+# reset the SPI bus for continued use. This code assumes power is lost on exit.
